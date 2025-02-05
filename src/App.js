7afa1a28713449925bcd15fd3bf62ad149f2f3df
Kratos1950 <87760385+Katos1950@users.noreply.github.com>
2025-02-04 20:15:15 -0600

    Text editor added with server

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { Dashboard } from "./Dashboard";
 import { Login } from "./Login/Login";
 import {SignUp} from './Login/SignUp';  
 import {Route,Routes, BrowserRouter as Router} from "react-router-dom"
+import { TextEditor } from "./textEditor/TextEditor";
 
 const App = () => {
   return (
@@ -10,6 +11,7 @@ const App = () => {
         <Route path="/" element={<Login/>}></Route>
         <Route path="/signup" element={<SignUp/>}></Route>
         <Route path="/dashboard" element={<Dashboard/>}></Route>
+        <Route path="/document/:id" element={<TextEditor/>}></Route>  
       </Routes>
     </Router>
   )
---
+++ src/textEditor/TextEditor.js
@@ -0,0 +1,99 @@
+import React, { useCallback, useEffect, useRef, useState } from 'react';
+import Quill from 'quill';
+import 'quill/dist/quill.snow.css';
+import { io } from "socket.io-client";
+import { useParams } from 'react-router-dom';
+import "./TextEditor.css";
+
+const TOOLBAR_OPTIONS = [
+  [{ header: [1, 2, 3, 4, 5, 6, false] }],
+  [{ font: [] }],
+  [{ list: "ordered" }, { list: "bullet" }],
+  ["bold", "italic", "underline"],
+  [{ color: [] }, { background: [] }],
+  [{ script: "sub" }, { script: "super" }],
+  ["image", "blockquote", "code-block"],
+  ["clean"]
+];
+
+export const TextEditor = () => {
+  const [socket, setSocket] = useState();
+  const [quill, setQuill] = useState();
+  const { id: documentId } = useParams();
+  const SAVE_INTERVAL_MS = 2000;
+
+  useEffect(() => {
+    const s = io("http://localhost:3001");
+    setSocket(s);
+
+    return () => {
+      s.disconnect();
+    };
+  }, []);
+
+  useEffect(() => {
+    if (!socket || !quill) return;
+
+    socket.once("load-document", content => {
+      quill.setContents(content); // Load Quill Delta object
+      quill.enable();
+  });
+
+    socket.emit("get-document", documentId);
+  }, [socket, quill, documentId]);
+
+  useEffect(() => {
+    if (!socket || !quill) return;
+
+    const handler = delta => {
+      quill.updateContents(delta);
+    };
+
+    socket.on("receive-changes", handler);
+
+    return () => {
+      socket.off("receive-changes", handler);
+    };
+  }, [socket, quill]);
+
+  useEffect(() => {
+    if (!socket || !quill) return;
+
+    const interval = setInterval(() => {
+      socket.emit("save-document", quill.getContents());
+    }, SAVE_INTERVAL_MS);
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, [socket, quill]);
+
+  useEffect(() => {
+    if (!socket || !quill) return;
+
+    const handler = (delta, oldDelta, source) => {
+      if (source !== "user") return;
+      socket.emit("send-changes", delta);
+    };
+
+    quill.on("text-change", handler);
+
+    return () => {
+      quill.off("text-change", handler);
+    };
+  }, [socket, quill]);
+
+  const editorRef = useCallback(wrapper => {
+    if (wrapper == null) return;
+
+    wrapper.innerHTML = "";
+    const editor = document.createElement("div");
+    wrapper.append(editor);
+    const q = new Quill(editor, { theme: "snow", modules: { toolbar: TOOLBAR_OPTIONS } });
+    q.disable();
+    q.setText("Loading...");
+    setQuill(q);
+  }, []);
+
+  return <div className='container' ref={editorRef}></div>;
+};
