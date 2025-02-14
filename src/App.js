3ef6e65413b343607c2d2284a3520a54201aacad
Kratos1950 <87760385+Katos1950@users.noreply.github.com>
2025-02-13 20:53:48 -0600

    temp forgot password page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { Dashboard } from "./Dashboard";
 import { Login } from "./Login/Login";
 import {SignUp} from './Login/SignUp';  
+import {ForgotPassword} from './Login/ForgotPassword';  
 import {Route,Routes, BrowserRouter as Router} from "react-router-dom"
 import { TextEditor } from "./textEditor/TextEditor";
 
@@ -12,6 +13,7 @@ const App = () => {
         <Route path="/signup" element={<SignUp/>}></Route>
         <Route path="/dashboard" element={<Dashboard/>}></Route>
         <Route path="/document/:id" element={<TextEditor/>}></Route>  
+        <Route path="/forgotpassword" element={<ForgotPassword/>}></Route>  
       </Routes>
     </Router>
   )
---
+++ src/Login/ForgotPassword.js
@@ -0,0 +1,109 @@
+import { useState } from "react";
+import "./Login.css";
+import ThreeD from "./ThreeD";
+import axios from "axios";
+import { Link, useNavigate } from "react-router-dom";
+import * as Yup from "yup";
+
+export const ForgotPassword = () => {
+  const [emailId, setEmailId] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [isVisible, setIsVisible] = useState(false);
+  const [errors, setErrors] = useState({});
+  const navigate = useNavigate()
+  const validationSchema = Yup.object({
+    password: Yup.string()
+      .required("Password is required")
+      .min(8, "Password must be at least 8 characters")
+      .matches(/[!@#$%^&*(),.?":{}|<>]/, "Must have at least one symbol")
+      .matches(/[0-9]/, "Must have at least one number")
+      .matches(/[A-Z]/, "Must have at least one uppercase letter")
+      .matches(/[a-z]/, "Must have at least one lowercase letter"),
+    confirmPassword: Yup.string()
+      .oneOf([Yup.ref("password")], "Passwords must match")
+      .required("Confirm your password"),
+  });
+
+  const handleResetPassword = async (e) => {
+    e.preventDefault();
+    setErrors({});
+
+    try {
+      await validationSchema.validate(
+        { emailId, password, confirmPassword },
+        { abortEarly: false }
+      );
+
+      const response = await axios.post("http://localhost:4000/users/resetpassword", {
+        emailId,
+        password,
+      });
+
+      if(response.status === 201){
+        navigate("/");
+      }
+      console.log(response.data);
+    } catch (error) {
+      const newErrors = {};
+      if (error.inner) {
+        error.inner.forEach((err) => {
+          newErrors[err.path] = err.message;
+        });
+      } else if (error.response && error.response.data) {
+        Object.keys(error.response.data).forEach((key) => {
+          newErrors[key] = error.response.data[key];
+        });
+      }
+      setErrors(newErrors);
+    }
+  };
+
+  return (
+    <div className="Page-container">
+      <div className="Image-container">
+        <ThreeD />
+      </div>
+
+      <div className="Login-container">
+        <form className="Form" onSubmit={handleResetPassword}>
+          <h1 className="text-3xl">Reset Password</h1>
+          <hr className="divider" />
+
+
+          <div className="input-container">
+            <i className="bi bi-lock px-1"></i>
+            <input
+              placeholder="Password"
+              type={isVisible ? "text" : "password"}
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            {errors.password && <p className="error">{errors.password}</p>}
+          </div>
+
+          <div className="input-container">
+            <i className="bi bi-file-earmark-lock-fill px-1"></i>
+            <input
+              placeholder="Re-type Password"
+              type={isVisible ? "text" : "password"}
+              value={confirmPassword}
+              onChange={(e) => setConfirmPassword(e.target.value)}
+            />
+            {errors.confirmPassword && <p className="error">{errors.confirmPassword}</p>}
+          </div>
+
+          <button type="submit" className="btn">
+            Register
+          </button>
+
+          <hr className="divider" />
+
+          <p>
+            Remember your password ? <Link to="/">Log In</Link>
+          </p>
+        </form>
+      </div>
+    </div>
+  );
+};
